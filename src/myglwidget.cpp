46563e7bb5954731319f5b59f7f46407e217a071
Stephan Schauerte <stephan.schauerte@ilt.fraunhofer.de>
2018-04-12 21:35:26 +0200

    Add MyGLWidget slots

--- src/myglwidget.cpp
+++ src/myglwidget.cpp
@@ -7,3 +7,35 @@ MyGLWidget::MyGLWidget(QWidget * parent) : QOpenGLWidget(parent) {
 MyGLWidget::~MyGLWidget() {
 
 }
+
+MyGLWidget::setFOV(int value) {
+
+}
+
+MyGLWidget::setAngle(int value) {
+
+}
+
+MyGLWidget::setProjectionMode() {
+
+}
+
+MyGLWidget::setNear(double value) {
+
+}
+
+MyGLWidget::setFar(double value) {
+
+}
+
+MyGLWidget::setRotationA(int value) {
+
+}
+
+MyGLWidget::setRotationB(int value) {
+
+}
+
+MyGLWidget::setRotationC(int value) {
+
+}
--- src/myglwidget.h
+++ src/myglwidget.h
@@ -5,10 +5,22 @@
 
 class MyGLWidget : public QOpenGLWidget {
 private:
+
 protected:
+
 public:
     MyGLWidget(QWidget * parent);
     ~MyGLWidget();
+
+public slots:
+    setFOV(int value);
+    setAngle(int value);
+    setProjectionMode();
+    setNear(double value);
+    setFar(double value);
+    setRotationA(int value);
+    setRotationB(int value);
+    setRotationC(int value);
 };
 
 #endif // MYGLWIDGET_H
